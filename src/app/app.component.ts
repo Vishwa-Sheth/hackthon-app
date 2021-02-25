1fadfc6c07defa8888699912cd56f618ca4ccd04
Vishwa-Sheth <shetvishwa3@gmail.com>
2021-02-25 22:06:11 +0530

    Picture is clicked using webcam and displayed in website

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,17 @@
 import { Component } from '@angular/core';
+import {Subject,Observable} from 'rxjs';
+import {WebcamImage} from 'ngx-webcam';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
+
 export class AppComponent {
   title = 'hackathon-gcp';
+  public webcamImage: WebcamImage = null;
+  handleImage(webcamImage: WebcamImage) {
+  this.webcamImage = webcamImage;
+  }
 }
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,15 +2,21 @@ import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
+import {FormsModule} from '@angular/forms';
 import { AppComponent } from './app.component';
+import { WebcamComponent } from './webcam/webcam.component';
+import {WebcamModule} from 'ngx-webcam';
+
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    WebcamComponent
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    WebcamModule
   ],
   providers: [],
   bootstrap: [AppComponent]
