f65e19731ff62f1951dc0d3db7e9309f72899fd3
Vishwa-Sheth <shetvishwa3@gmail.com>
2021-02-26 03:06:39 +0530

    installed i18n and updated module.ts

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,11 +1,19 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 
+import {TranslateModule,TranslateLoader} from '@ngx-translate/core';
+import {TranslateHttpLoader} from '@ngx-translate/http-loader';
 import { AppRoutingModule } from './app-routing.module';
 import {FormsModule} from '@angular/forms';
 import { AppComponent } from './app.component';
 import { WebcamComponent } from './webcam/webcam.component';
 import {WebcamModule} from 'ngx-webcam';
+import { HttpClientModule,HttpClient } from '@angular/common/http';
+
+
+export function HttpLoaderFactory(http: HttpClient){
+  return new TranslateHttpLoader(http,"angular-i18n")
+}
 
 
 @NgModule({
@@ -16,7 +24,15 @@ import {WebcamModule} from 'ngx-webcam';
   imports: [
     BrowserModule,
     AppRoutingModule,
-    WebcamModule
+    WebcamModule,
+    HttpClientModule,
+    TranslateModule.forRoot({
+      loader:{
+        provide: TranslateLoader,
+        useFactory: HttpLoaderFactory,
+        deps: [HttpClient]
+      }
+    })
   ],
   providers: [],
   bootstrap: [AppComponent]
