ad8b2f3022bb33f2aeec4906b790c27298e09c87
Vishwa-Sheth <shetvishwa3@gmail.com>
2021-02-26 06:29:59 +0530

    Basic UI template integrated

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -9,6 +9,25 @@ import { AppComponent } from './app.component';
 import { WebcamComponent } from './webcam/webcam.component';
 import {WebcamModule} from 'ngx-webcam';
 import { HttpClientModule,HttpClient } from '@angular/common/http';
+import { HomeComponent } from './home/home.component';
+import { LandingComponent } from './landing/landing.component';
+import { ProfileComponent } from './profile/profile.component';
+import { LoginComponent } from './login/login.component';
+import { AlertsSectionComponent } from './sections/alerts-section/alerts-section.component';
+import { ButtonsSectionComponent } from './sections/buttons-section/buttons-section.component';
+import { CrsSectionComponent } from './sections/crs-section/crs-section.component';
+import { InputsSectionComponent } from './sections/inputs-section/inputs-section.component';
+import { ModalComponent } from './sections/modal/modal.component';
+import { NavigationSectionComponent } from './sections/navigation-section/navigation-section.component';
+import { NucleoSectionComponent } from './sections/nucleo-section/nucleo-section.component';
+import { TabsSectionComponent } from './sections/tabs-section/tabs-section.component';
+import { TypographySectionComponent } from './sections/typography-section/typography-section.component';
+import { VersionsSectionComponent } from './sections/versions-section/versions-section.component';
+import { SectionsComponent } from './sections/sections.component';
+import { FooterComponent } from './shared/footer/footer.component';
+import { NavbarComponent } from './shared/navbar/navbar.component';
+import { SignupComponent } from './signup/signup.component';
+import { NgbModule } from '@ng-bootstrap/ng-bootstrap';
 
 
 export function HttpLoaderFactory(http: HttpClient){
@@ -19,7 +38,25 @@ export function HttpLoaderFactory(http: HttpClient){
 @NgModule({
   declarations: [
     AppComponent,
-    WebcamComponent
+    WebcamComponent,
+    HomeComponent,
+    LandingComponent,
+    ProfileComponent,
+    LoginComponent,
+    AlertsSectionComponent,
+    ButtonsSectionComponent,
+    CrsSectionComponent,
+    InputsSectionComponent,
+    ModalComponent,
+    NavigationSectionComponent,
+    NucleoSectionComponent,
+    TabsSectionComponent,
+    TypographySectionComponent,
+    VersionsSectionComponent,
+    SectionsComponent,
+    FooterComponent,
+    NavbarComponent,
+    SignupComponent
   ],
   imports: [
     BrowserModule,
@@ -32,7 +69,8 @@ export function HttpLoaderFactory(http: HttpClient){
         useFactory: HttpLoaderFactory,
         deps: [HttpClient]
       }
-    })
+    }),
+    NgbModule
   ],
   providers: [],
   bootstrap: [AppComponent]
